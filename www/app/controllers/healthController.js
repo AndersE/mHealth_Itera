d43c6ced57560e3fd57653ef7db9cc3c2b25a539
erik.berg.solheim <erik.berg.solheim@gmail.com>
2014-05-28 13:32:15 +0200

    Yes sir

--- www/app/controllers/healthController.js
+++ www/app/controllers/healthController.js
@@ -3,8 +3,50 @@
     $scope.challengeQuestions = jsonService.challenge;
     $scope.foodQuestions = jsonService.food;
     $scope.restedQuestions = jsonService.rested;
-    $scope.happynessQuestions = jsonService.happyness;
+    $scope.happinessQuestions = jsonService.happiness;
     $scope.progressbarValue = 40;
     $scope.progressbarType = calculateService.calculateType($scope.progressbarValue);
+    $scope.isChallengeExpanded = false;
+    $scope.isFoodExpanded = false;
+    $scope.isRestedExpanded = false;
+    $scope.isHappinessExpanded = false;
+
+    $('#ex1').slider({
+        formater: function(value) {
+            return 'Current value: ' + value;
+        }
+    });
+
+    $scope.expandChallenge = function() {
+        if ($scope.isChallengeExpanded) {
+            $scope.isChallengeExpanded = false;
+        } else {
+            $scope.isChallengeExpanded = true;
+        }
+    };
+
+    $scope.expandFood = function() {
+        if ($scope.isFoodExpanded) {
+            $scope.isFoodExpanded = false;
+        } else {
+            $scope.isFoodExpanded = true;
+        }
+    };
+
+    $scope.expandRested = function() {
+        if ($scope.isRestedExpanded) {
+            $scope.isRestedExpanded = false;
+        } else {
+            $scope.isRestedExpanded = true;
+        }
+    };
+
+    $scope.expandHappiness = function() {
+        if ($scope.isHappinessExpanded) {
+            $scope.isHappinessExpanded = false;
+        } else {
+            $scope.isHappinessExpanded = true;
+        }
+    };
 });
 
--- www/app/services/jsonService.js
+++ www/app/services/jsonService.js
@@ -2,33 +2,24 @@ angular.module('HealthApp.json', []).
     factory('jsonService', function() {
         return {
             challenge: [
-                {
-                    "Sporsmal1": "Tekst for spørsmål1",
-                    "Sporsmal2": "Tekst for spørsmål2",
-                    "Sporsmal3": "Tekst for spørsmål3"
-                }
+                {"Sporsmal": "Tekst for spørsmål1"},
+                {"Sporsmal": "Tekst for spørsmål2"},
+                {"Sporsmal": "Tekst for spørsmål3"}
             ],
-
             food: [
-                {
-                    "Sporsmal1": "Tekst for spørsmål1",
-                    "Sporsmal2": "Tekst for spørsmål2",
-                    "Sporsmal3": "Tekst for spørsmål3"
-                }
+                {"Sporsmal": "Tekst for spørsmål1"},
+                {"Sporsmal": "Tekst for spørsmål2"},
+                {"Sporsmal": "Tekst for spørsmål3"}
             ],
             rested: [
-                {
-                    "Sporsmal1": "Tekst for spørsmål1",
-                    "Sporsmal2": "Tekst for spørsmål2",
-                    "Sporsmal3": "Tekst for spørsmål3"
-                }
+                {"Sporsmal": "Tekst for spørsmål1"},
+                {"Sporsmal": "Tekst for spørsmål2"},
+                {"Sporsmal": "Tekst for spørsmål3"}
             ],
-            happyness: [
-                {
-                    "Sporsmal1": "Tekst for spørsmål1",
-                    "Sporsmal2": "Tekst for spørsmål2",
-                    "Sporsmal3": "Tekst for spørsmål3"
-                }
+            happiness: [
+                {"Sporsmal": "Tekst for spørsmål1"},
+                {"Sporsmal": "Tekst for spørsmål2"},
+                {"Sporsmal": "Tekst for spørsmål3"}
             ]
         }
     });
