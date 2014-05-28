2985588f7d7e58df9bb0153ff5cff0ff2d59fb1e
erik.berg.solheim <erik.berg.solheim@gmail.com>
2014-05-28 12:49:10 +0200

    Yes sir

--- www/app/controllers/healthController.js
+++ www/app/controllers/healthController.js
@@ -1,6 +1,6 @@
 ﻿HealthApp.controller('HealthCtrl', function ($scope,  healthService, jsonService, calculateService) {
     $scope.helloWorld = "Controller speaking: Hello world";
-    $scope.challengeQuestions = jsonService.challaege;
+    $scope.challengeQuestions = jsonService.challenge;
     $scope.foodQuestions = jsonService.food;
     $scope.restedQuestions = jsonService.rested;
     $scope.happynessQuestions = jsonService.happyness;
